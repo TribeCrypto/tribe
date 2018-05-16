3ee09fb74cc8f6c2986dfe884b51d1737b154494
blocksninja <183516+blocksninja@users.noreply.github.com>
2018-05-16 16:18:55 +0200

    added default explorer url. (#5)

--- src/qt/optionsmodel.cpp
+++ src/qt/optionsmodel.cpp
@@ -69,7 +69,7 @@ void OptionsModel::Init(bool resetSettings)
     nDisplayUnit = settings.value("nDisplayUnit").toInt();
 
     if (!settings.contains("strThirdPartyTxUrls"))
-        settings.setValue("strThirdPartyTxUrls", "");
+        settings.setValue("strThirdPartyTxUrls", "https://explorer.tribe-crypto.com/tx/%s");
     strThirdPartyTxUrls = settings.value("strThirdPartyTxUrls", "").toString();
 
     if (!settings.contains("fCoinControlFeatures"))
