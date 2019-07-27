c94af855d193043f8f1e942785f62221c1e59d56
denhag <thegboxer@gmail.com>
2019-07-27 17:51:15 +0200

    Mandatory update

--- src/version.h
+++ src/version.h
@@ -33,7 +33,7 @@ static const int MIN_MNW_PEER_PROTO_VERSION = 70400;
 //! minimum peer version that can receive masternode payments
 // V1 - Last protocol version before update
 // V2 - Newest protocol version
-static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 70300;
+static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_1 = 70201;
 static const int MIN_MASTERNODE_PAYMENT_PROTO_VERSION_2 = 70400;
 
 //! nTime field added to CAddress, starting with this version;
